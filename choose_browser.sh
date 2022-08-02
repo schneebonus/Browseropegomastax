349e26246c06fa1de13e5fc4c478f5fa0e0caf53
Mark Schneemann <schneebonus@users.noreply.github.com>
2022-08-02 15:58:26 +0200

    Update choose_browser.sh

--- choose_browser.sh
+++ choose_browser.sh
@@ -1,8 +1,8 @@
 # CONFIG
 
 # Add your browser run commands here.
-# Separated by spaces.
-browsers="firefox tor-browser-en chromium google-chrome-stable"
+# Separated by commas.
+browsers="firefox,tor-browser-en,chromium,google-chrome-stable,clipboard"
 
 # Set rofi theme
 # Might help to run "rofi-theme-selector".
@@ -18,7 +18,7 @@ if [ $# -eq 0 ]
 fi
 
 # building rofi choices from browsers
-choices=${browsers[*]// /\\n}
+choices=${browsers[*]//,/\\n}
 
 # let rofi ask for your choice and handle the url
 echo -e $choices | $(rofi -dmenu -mesg $1 -theme $rofi_theme) $1
